92086784b593334a70fa541a401ee913f8a16580
wnsgh <junho767@naver.com>
2025-02-28 17:59:49 +0900

    chore: backend project setting

---
+++ backend/src/main/java/com/java/NBE4_5_1_7/Nbe4517Application.java
@@ -0,0 +1,13 @@
+package com.java.NBE4_5_1_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe4517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbe4517Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/java/NBE4_5_1_7/Nbe4517ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.NBE4_5_1_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe4517ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
