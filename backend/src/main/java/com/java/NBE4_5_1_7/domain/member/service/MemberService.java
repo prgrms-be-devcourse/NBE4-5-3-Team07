7417a0b92689244ce0b354c54ae37288d5e17613
choijaewoo <cchoi00332244@gmail.com>
2025-03-05 14:45:52 +0900

    member service - 쿠키에서 member ID 추출 하는 방식 채택, 해당 메서드 생성

--- backend/src/main/java/com/java/NBE4_5_1_7/domain/member/service/MemberService.java
+++ backend/src/main/java/com/java/NBE4_5_1_7/domain/member/service/MemberService.java
@@ -66,8 +66,8 @@ public class MemberService {
         );
     }
 
-    public Long getIdFromMember(Optional<Member> optionalMember) {
-        Member member = optionalMember.orElseThrow(() -> new RuntimeException("해당 사용자를 찾을 수 없습니다."));
+    public Long getIdFromMember(String accessToken) {
+        Member member = getMemberByAccessToken(accessToken).orElseThrow(() -> new RuntimeException("해당 사용자를 찾을 수 없습니다."));
         return member.getId();
     }
 
