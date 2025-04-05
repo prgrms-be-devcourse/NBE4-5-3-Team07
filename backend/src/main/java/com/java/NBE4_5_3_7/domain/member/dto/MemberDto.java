c8652a32d0eef40ed4fb0f2918fd7933ceec3a74
Hanjise0ng <tjdwl3269@gmail.com>
2025-04-06 00:20:24 +0900

    feat: Member Delombok

--- backend/src/main/java/com/java/NBE4_5_3_7/domain/member/dto/MemberDto.java
+++ backend/src/main/java/com/java/NBE4_5_3_7/domain/member/dto/MemberDto.java
@@ -1,23 +1,21 @@
 package com.java.NBE4_5_3_7.domain.member.dto;
 
 import com.java.NBE4_5_3_7.domain.member.entity.Member;
-import lombok.Getter;
 import org.springframework.lang.NonNull;
 
 import java.time.format.DateTimeFormatter;
 
-@Getter
 public class MemberDto {
     @NonNull
-    private long id;
+    private final long id;
     @NonNull
-    private String nickname;
+    private final String nickname;
     @NonNull
-    private String profileImgUrl;
+    private final String profileImgUrl;
     @NonNull
-    private String subscriptPlan;
+    private final String subscriptPlan;
     @NonNull
-    private String subscribeEndDate;
+    private final String subscribeEndDate;
 
     public MemberDto(Member member) {
         this.id = member.getId();
@@ -26,5 +24,30 @@ public class MemberDto {
         this.subscriptPlan = member.getSubscriptionPlan().toString();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         this.subscribeEndDate = member.getSubscribeEndDate().format(formatter);
+    }
+
+    @NonNull
+    public long getId() {
+        return this.id;
+    }
+
+    @NonNull
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    @NonNull
+    public String getProfileImgUrl() {
+        return this.profileImgUrl;
+    }
+
+    @NonNull
+    public String getSubscriptPlan() {
+        return this.subscriptPlan;
+    }
+
+    @NonNull
+    public String getSubscribeEndDate() {
+        return this.subscribeEndDate;
     }
 }
--- backend/src/main/java/com/java/NBE4_5_3_7/domain/member/entity/Member.java
+++ backend/src/main/java/com/java/NBE4_5_3_7/domain/member/entity/Member.java
@@ -3,11 +3,6 @@ package com.java.NBE4_5_3_7.domain.member.entity;
 import com.java.NBE4_5_3_7.domain.interview.entity.InterviewContentBookmark;
 import com.java.NBE4_5_3_7.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,13 +13,8 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@SuperBuilder
 @EntityListeners(AuditingEntityListener.class)
-public class Member extends BaseEntity{
+public class Member extends BaseEntity {
 
     @Column(length = 100, unique = true)
     private String username;
@@ -47,6 +37,36 @@ public class Member extends BaseEntity{
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<InterviewContentBookmark> bookmarks = new ArrayList<>();
 
+    public Member(String username, String apiKey, String nickname, String profileImgUrl, Role role, SubscriptionPlan subscriptionPlan, LocalDateTime subscribeEndDate, List<InterviewContentBookmark> bookmarks) {
+        this.username = username;
+        this.apiKey = apiKey;
+        this.nickname = nickname;
+        this.profileImgUrl = profileImgUrl;
+        this.role = role;
+        this.subscriptionPlan = subscriptionPlan;
+        this.subscribeEndDate = subscribeEndDate;
+        this.bookmarks = bookmarks;
+    }
+
+    public Member() {
+    }
+
+    protected Member(MemberBuilder<?, ?> b) {
+        super(b);
+        this.username = b.username;
+        this.apiKey = b.apiKey;
+        this.nickname = b.nickname;
+        this.profileImgUrl = b.profileImgUrl;
+        this.role = b.role;
+        this.subscriptionPlan = b.subscriptionPlan;
+        this.subscribeEndDate = b.subscribeEndDate;
+        this.bookmarks = b.bookmarks;
+    }
+
+    public static MemberBuilder<?, ?> builder() {
+        return new MemberBuilderImpl();
+    }
+
     public boolean isAdmin() {
         return Role.ADMIN.equals(role);
     }
@@ -66,7 +86,7 @@ public class Member extends BaseEntity{
 
         authorities.add("ROLE_USER");
 
-        if(isAdmin()) {
+        if (isAdmin()) {
             authorities.add("ROLE_ADMIN");
         } else {
             authorities.add("ROLE_USER");
@@ -81,5 +101,141 @@ public class Member extends BaseEntity{
 
     public String getProfileImgUrlOrDefaultUrl() {
         return (profileImgUrl == null || profileImgUrl.isBlank()) ? "https://placehold.co/640x640?text=O_O" : this.profileImgUrl;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getApiKey() {
+        return this.apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getProfileImgUrl() {
+        return this.profileImgUrl;
+    }
+
+    public void setProfileImgUrl(String profileImgUrl) {
+        this.profileImgUrl = profileImgUrl;
+    }
+
+    public Role getRole() {
+        return this.role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public SubscriptionPlan getSubscriptionPlan() {
+        return this.subscriptionPlan;
+    }
+
+    public void setSubscriptionPlan(SubscriptionPlan subscriptionPlan) {
+        this.subscriptionPlan = subscriptionPlan;
+    }
+
+    public LocalDateTime getSubscribeEndDate() {
+        return this.subscribeEndDate;
+    }
+
+    public void setSubscribeEndDate(LocalDateTime subscribeEndDate) {
+        this.subscribeEndDate = subscribeEndDate;
+    }
+
+    public List<InterviewContentBookmark> getBookmarks() {
+        return this.bookmarks;
+    }
+
+    public void setBookmarks(List<InterviewContentBookmark> bookmarks) {
+        this.bookmarks = bookmarks;
+    }
+
+    public static abstract class MemberBuilder<C extends Member, B extends MemberBuilder<C, B>> extends BaseEntityBuilder<C, B> {
+        private String username;
+        private String apiKey;
+        private String nickname;
+        private String profileImgUrl;
+        private Role role;
+        private SubscriptionPlan subscriptionPlan;
+        private LocalDateTime subscribeEndDate;
+        private List<InterviewContentBookmark> bookmarks;
+
+        public B username(String username) {
+            this.username = username;
+            return self();
+        }
+
+        public B apiKey(String apiKey) {
+            this.apiKey = apiKey;
+            return self();
+        }
+
+        public B nickname(String nickname) {
+            this.nickname = nickname;
+            return self();
+        }
+
+        public B profileImgUrl(String profileImgUrl) {
+            this.profileImgUrl = profileImgUrl;
+            return self();
+        }
+
+        public B role(Role role) {
+            this.role = role;
+            return self();
+        }
+
+        public B subscriptionPlan(SubscriptionPlan subscriptionPlan) {
+            this.subscriptionPlan = subscriptionPlan;
+            return self();
+        }
+
+        public B subscribeEndDate(LocalDateTime subscribeEndDate) {
+            this.subscribeEndDate = subscribeEndDate;
+            return self();
+        }
+
+        public B bookmarks(List<InterviewContentBookmark> bookmarks) {
+            this.bookmarks = bookmarks;
+            return self();
+        }
+
+        protected abstract B self();
+
+        public abstract C build();
+
+        public String toString() {
+            return "Member.MemberBuilder(super=" + super.toString() + ", username=" + this.username + ", apiKey=" + this.apiKey + ", nickname=" + this.nickname + ", profileImgUrl=" + this.profileImgUrl + ", role=" + this.role + ", subscriptionPlan=" + this.subscriptionPlan + ", subscribeEndDate=" + this.subscribeEndDate + ", bookmarks=" + this.bookmarks + ")";
+        }
+    }
+
+    private static final class MemberBuilderImpl extends MemberBuilder<Member, MemberBuilderImpl> {
+        private MemberBuilderImpl() {
+        }
+
+        protected MemberBuilderImpl self() {
+            return this;
+        }
+
+        public Member build() {
+            return new Member(this);
+        }
     }
 }
