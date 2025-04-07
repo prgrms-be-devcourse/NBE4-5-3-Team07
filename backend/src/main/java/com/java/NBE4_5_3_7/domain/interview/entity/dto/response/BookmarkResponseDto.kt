5cbc037193cbdd0ece9f3cfeee6e5a54c1d886d5
Hanjise0ng <tjdwl3269@gmail.com>
2025-04-07 22:09:56 +0900

    fix: BookmarkResponseDto 기본값 추가

--- backend/src/main/java/com/java/NBE4_5_3_7/domain/interview/entity/dto/response/BookmarkResponseDto.kt
+++ backend/src/main/java/com/java/NBE4_5_3_7/domain/interview/entity/dto/response/BookmarkResponseDto.kt
@@ -1,7 +1,7 @@
 package com.java.NBE4_5_3_7.domain.interview.entity.dto.response
 
 data class BookmarkResponseDto(
-    var contentId: Long,
-    var question: String,
-    var answer: String
+    var contentId: Long = 0L,
+    var question: String = "",
+    var answer: String = ""
 )
