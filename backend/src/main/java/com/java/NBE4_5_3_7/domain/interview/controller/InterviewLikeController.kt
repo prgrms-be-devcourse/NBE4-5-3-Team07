e673113873fec1d10aa87efb3f75afcbbfa307b0
Hanjise0ng <tjdwl3269@gmail.com>
2025-04-07 22:21:49 +0900

    feat: Add tests for InterviewLikeControllerTest

--- backend/src/main/java/com/java/NBE4_5_3_7/domain/interview/controller/InterviewLikeController.kt
+++ backend/src/main/java/com/java/NBE4_5_3_7/domain/interview/controller/InterviewLikeController.kt
@@ -2,6 +2,7 @@ package com.java.NBE4_5_3_7.domain.interview.controller
 
 import com.java.NBE4_5_3_7.domain.interview.service.InterviewLikeService
 import com.java.NBE4_5_3_7.domain.member.service.MemberService
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +16,7 @@ class InterviewLikeController(
     private val memberService: MemberService
 ) {
 
-    @GetMapping("")
+    @GetMapping("", produces = [MediaType.TEXT_PLAIN_VALUE + ";charset=UTF-8"])
     fun likeChange(@RequestParam("id") id: Long): ResponseEntity<String> {
         val result = likeService.interviewLike(memberService.getIdFromRq(), id)
         return ResponseEntity.ok(result)
