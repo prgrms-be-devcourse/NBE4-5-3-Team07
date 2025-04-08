0361787a472317b894984a16d5ad7a1516bb4689
ohnoesganj <sunho3456@naver.com>
2025-04-08 11:28:55 +0900

    refactor: remove code comment

--- backend/src/main/java/com/java/NBE4_5_3_7/global/config/RedisConfig.kt
+++ backend/src/main/java/com/java/NBE4_5_3_7/global/config/RedisConfig.kt
@@ -18,7 +18,7 @@ import java.nio.charset.StandardCharsets
 class RedisConfig(
     @param:Lazy private val chatSubscriber: ChatSubscriber,
     @param:Lazy private val chatPublisher: ChatPublisher) {
-    // RedisTemplate을 생성하여 Redis와 데이터를 송수신할 수 있도록 설정
+
     @Bean
     fun redisTemplate(connectionFactory: RedisConnectionFactory?): RedisTemplate<String, Message> {
         val template = RedisTemplate<String, Message>()
@@ -28,13 +28,11 @@ class RedisConfig(
         return template
     }
 
-    // Redis Pub/Sub을 구독하여 유저와 관리자 메시지를 처리하는 리스너 설정
     @Bean
     fun redisMessageListenerContainer(connectionFactory: RedisConnectionFactory): RedisMessageListenerContainer {
         val container = RedisMessageListenerContainer()
         container.connectionFactory = connectionFactory
 
-        // 유저 메시지를 구독하여 WebSocket을 통해 관리자에게 전달
         container.addMessageListener({ message: org.springframework.data.redis.connection.Message, pattern: ByteArray? ->
             val channel = String(message.channel, StandardCharsets.UTF_8)
             val messageContent = String(message.body, StandardCharsets.UTF_8)
@@ -45,12 +43,10 @@ class RedisConfig(
             }
         }, PatternTopic("chat:*"))
 
-        // 관리자 메시지를 구독하여 WebSocket을 통해 유저에게 전달
         container.addMessageListener({ message: org.springframework.data.redis.connection.Message, pattern: ByteArray? ->
             val channel = String(message.channel, StandardCharsets.UTF_8)
             val messageContent = String(message.body, StandardCharsets.UTF_8)
-            println("✅ [RedisConfig] 관리자 메시지 수신 - 채널: $channel | 메시지: $messageContent")
-            chatSubscriber.forwardAdminMessageToRoom(messageContent, channel) // ChatSubscriber가 직접 WebSocket으로 메시지 전송
+            chatSubscriber.forwardAdminMessageToRoom(messageContent, channel)
         }, PatternTopic("admin:chat:*"))
 
         return container
