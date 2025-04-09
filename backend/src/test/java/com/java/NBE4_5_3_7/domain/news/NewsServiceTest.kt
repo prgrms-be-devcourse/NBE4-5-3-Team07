e6d71cabe50d3a8ab222324ede189e34cfd3feba
junho767 <junho767@naver.com>
2025-04-09 11:46:32 +0900

    test: Payment 도메인 테스트 케이스 코드 추가

--- backend/src/test/java/com/java/NBE4_5_3_7/domain/news/NewsServiceTest.kt
+++ backend/src/test/java/com/java/NBE4_5_3_7/domain/news/NewsServiceTest.kt
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 class NewsServiceTest {
 
     @Autowired
